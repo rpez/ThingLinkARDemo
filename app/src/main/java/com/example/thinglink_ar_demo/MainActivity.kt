e996c3a9c0297df9e94d51f92342ba69d5f3ab19
Reetu Kontio <reetu.kontio@gmail.com>
2023-10-19 15:28:17 +0300

    Start restructuring layout

--- app/src/main/java/com/example/thinglink_ar_demo/MainActivity.kt
+++ app/src/main/java/com/example/thinglink_ar_demo/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.thinglink_ar_demo
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -12,9 +15,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.core.content.ContextCompat
 import com.example.thinglink_ar_demo.common.helpers.CameraPermissionHelper
 import com.example.thinglink_ar_demo.common.helpers.SnackbarHelper
 import com.example.thinglink_ar_demo.ui.theme.ThingLinkARDemoTheme
+import com.google.android.material.snackbar.Snackbar
 import com.google.ar.core.ArCoreApk
 import com.google.ar.core.AugmentedImage
 import com.google.ar.core.AugmentedImageDatabase
@@ -60,101 +65,174 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Box(modifier = Modifier.fillMaxSize()){
-//                        ARScreen(session)
+                        ARScreen(session)
                     }
                 }
             }
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-
-        if (shouldCreateSession) {
-            var exception: Exception? = null
-            var message: String? = null
-            try {
-                when (ArCoreApk.getInstance().requestInstall(this, !installRequested)) {
-                    ArCoreApk.InstallStatus.INSTALL_REQUESTED -> {
-                        installRequested = true
-                        return
-                    }
-
-                    ArCoreApk.InstallStatus.INSTALLED -> {}
-                }
-
-                // ARCore requires camera permissions to operate. If we did not yet obtain runtime
-                // permission on Android M and above, now is a good time to ask the user for it.
-                if (!CameraPermissionHelper.hasCameraPermission(this)) {
-                    CameraPermissionHelper.requestCameraPermission(this)
-                    return
-                }
-                session = Session(this)
-            } catch (e: UnavailableArcoreNotInstalledException) {
-                message = "Please install ARCore"
-                exception = e
-            } catch (e: UnavailableUserDeclinedInstallationException) {
-                message = "Please install ARCore"
-                exception = e
-            } catch (e: UnavailableApkTooOldException) {
-                message = "Please update ARCore"
-                exception = e
-            } catch (e: UnavailableSdkTooOldException) {
-                message = "Please update this app"
-                exception = e
-            } catch (e: Exception) {
-                message = "This device does not support AR"
-                exception = e
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                Log.i("Permission: ", "Granted")
+            } else {
+                Log.i("Permission: ", "Denied")
             }
-            if (message != null) {
-                messageSnackbarHelper.showError(this, message)
-                Log.e("Error", "Exception creating session", exception)
-                return
+        }
+
+    fun View.showSnackbar(
+        view: View,
+        msg: String,
+        length: Int,
+        actionMessage: CharSequence?,
+        action: (View) -> Unit
+    ) {
+        val snackbar = Snackbar.make(view, msg, length)
+        if (actionMessage != null) {
+            snackbar.setAction(actionMessage) {
+                action(this)
+            }.show()
+        } else {
+            snackbar.show()
+        }
+    }
+
+    fun requestPermission(view: View) {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                showSnackbar(
+                    view,
+                    getString(R.string.permission_granted),
+                    Snackbar.LENGTH_INDEFINITE,
+                    null
+                ) {}
             }
-            shouldConfigureSession = true
-            shouldCreateSession = false
-        }
-        if (shouldConfigureSession) {
-            configureSession()
-            shouldConfigureSession = false
-        }
 
-        // Note that order matters - see the note in onPause(), the reverse applies here.
-        try {
-            session.resume()
-        } catch (e: CameraNotAvailableException) {
-            messageSnackbarHelper.showError(this, "Camera not available. Try restarting the app.")
-            session.close()
-            return
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.CAMERA
+            ) -> {
+                layout.showSnackbar(
+                    view,
+                    getString(R.string.permission_required),
+                    Snackbar.LENGTH_INDEFINITE,
+                    getString(R.string.ok)
+                ) {
+                    requestPermissionLauncher.launch(
+                        Manifest.permission.CAMERA
+                    )
+                }
+            }
+
+            else -> {
+                requestPermissionLauncher.launch(
+                    Manifest.permission.CAMERA
+                )
+            }
         }
-//        surfaceView.onResume()
-//        displayRotationHelper.onResume()
-//        fitToScanView.setVisibility(View.VISIBLE)
     }
 
-    override fun onPause() {
-        super.onPause()
-        shouldCreateSession = true
-    }
+//    override fun onResume() {
+//        super.onResume()
+//
+//        if (shouldCreateSession) {
+//            var exception: Exception? = null
+//            var message: String? = null
+//            try {
+//                when (ArCoreApk.getInstance().requestInstall(this, !installRequested)) {
+//                    ArCoreApk.InstallStatus.INSTALL_REQUESTED -> {
+//                        installRequested = true
+//                        return
+//                    }
+//
+//                    ArCoreApk.InstallStatus.INSTALLED -> {}
+//                }
+//
+//                // ARCore requires camera permissions to operate. If we did not yet obtain runtime
+//                // permission on Android M and above, now is a good time to ask the user for it.
+//                if (!CameraPermissionHelper.hasCameraPermission(this)) {
+//                    CameraPermissionHelper.requestCameraPermission(this)
+//                    return
+//                }
+//                session = Session(this)
+//            } catch (e: UnavailableArcoreNotInstalledException) {
+//                message = "Please install ARCore"
+//                exception = e
+//            } catch (e: UnavailableUserDeclinedInstallationException) {
+//                message = "Please install ARCore"
+//                exception = e
+//            } catch (e: UnavailableApkTooOldException) {
+//                message = "Please update ARCore"
+//                exception = e
+//            } catch (e: UnavailableSdkTooOldException) {
+//                message = "Please update this app"
+//                exception = e
+//            } catch (e: Exception) {
+//                message = "This device does not support AR"
+//                exception = e
+//            }
+//            if (message != null) {
+//                messageSnackbarHelper.showError(this, message)
+//                Log.e("Error", "Exception creating session", exception)
+//                return
+//            }
+//            shouldConfigureSession = true
+//            shouldCreateSession = false
+//        }
+//        if (shouldConfigureSession) {
+//            configureSession()
+//            shouldConfigureSession = false
+//        }
+//
+//        // Note that order matters - see the note in onPause(), the reverse applies here.
+//        try {
+//            session.resume()
+//        } catch (e: CameraNotAvailableException) {
+//            messageSnackbarHelper.showError(this, "Camera not available. Try restarting the app.")
+//            session.close()
+//            return
+//        }
+////        surfaceView.onResume()
+////        displayRotationHelper.onResume()
+////        fitToScanView.setVisibility(View.VISIBLE)
+//    }
 
-    private fun configureSession() {
-        config = Config(session)
-        config.focusMode = Config.FocusMode.AUTO
-        if (!setupAugmentedImageDatabase(config)) {
-            messageSnackbarHelper.showError(this, "Could not setup augmented image database")
-        }
-        session.configure(config)
-    }
+//    override fun onPause() {
+//        super.onPause()
+//        shouldCreateSession = true
+//        session.pause()
+//    }
 
-    private fun setupAugmentedImageDatabase(config: Config): Boolean {
-        imageDatabase = this.assets.open("imagedb/images.imgdb").use {
-            AugmentedImageDatabase.deserialize(session, it)
-        }
-        if (imageDatabase.numImages == 0) return false
+//    override fun onDestroy() {
+//        session.close()
+//        shouldCreateSession = true
+//        super.onDestroy()
+//    }
 
-        config.augmentedImageDatabase = imageDatabase
-        return true
-    }
+//    private fun configureSession() {
+//        config = Config(session)
+//        config.focusMode = Config.FocusMode.AUTO
+//        if (!setupAugmentedImageDatabase(config)) {
+//            messageSnackbarHelper.showError(this, "Could not setup augmented image database")
+//        }
+//        session.configure(config)
+//    }
+//
+//    private fun setupAugmentedImageDatabase(config: Config): Boolean {
+//        imageDatabase = this.assets.open("imagedb/images.imgdb").use {
+//            AugmentedImageDatabase.deserialize(session, it)
+//        }
+//        if (imageDatabase.numImages == 0) return false
+//
+//        config.augmentedImageDatabase = imageDatabase
+//        return true
+//    }
 }
 
 @Composable
