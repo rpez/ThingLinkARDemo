0943a339fbbb26c8654b8eb96079e7c47646313c
Reetu Kontio <reetu.kontio@gmail.com>
2023-10-22 00:40:42 +0300

    add popup window

--- app/src/main/java/com/example/thinglink_ar_demo/MainActivity.kt
+++ app/src/main/java/com/example/thinglink_ar_demo/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.thinglink_ar_demo
 
 import android.Manifest
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
@@ -11,8 +12,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -20,22 +26,28 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.mediumTopAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Popup
 import com.example.thinglink_ar_demo.ui.theme.ThingLinkARDemoTheme
 import com.google.ar.core.Config.LightEstimationMode
 import com.google.ar.core.Session
+import com.google.ar.sceneform.math.Vector3
 import io.github.sceneview.ar.ARScene
+import io.github.sceneview.ar.arcore.position
 import io.github.sceneview.ar.node.ArModelNode
 import io.github.sceneview.ar.node.ArNode
 import io.github.sceneview.ar.node.PlacementMode
+import io.github.sceneview.math.toVector3
 
 class MainActivity : ComponentActivity() {
 
+    private val objectTapRadius: Float = 1.0f
     private lateinit var session: Session
 
     // Coroutine launcher for permissions
@@ -66,6 +78,8 @@ class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun MainView() {
+        val showPopup = remember { mutableStateOf<Boolean>(false) }
+
         Scaffold(
             topBar = {
                 TopAppBar(
@@ -104,7 +118,8 @@ class MainActivity : ComponentActivity() {
                     Box(
                         modifier = Modifier.fillMaxSize()
                     ) {
-                        ARScreen()
+                        ARScreen(showPopup)
+                        if (showPopup.value) InfoPopup(showPopup)
                     }
                 }
             }
@@ -113,9 +128,11 @@ class MainActivity : ComponentActivity() {
 
 //    @Preview
     @Composable
-    fun ARScreen() {
+    fun ARScreen(showPopup: MutableState<Boolean>) {
         val arNodes = remember { mutableListOf<ArNode>() }
         val modelNode = remember { mutableStateOf<ArModelNode?>(null) }
+        val modelPlaced = remember { mutableStateOf<Boolean>(false) }
+        val nodePosition = remember { mutableStateOf<Vector3>(Vector3.zero()) }
 
         // Construct AR scene
         ARScene(
@@ -133,10 +150,59 @@ class MainActivity : ComponentActivity() {
                     }
                 arNodes.add(modelNode.value!!)
             },
-            onTap = { _ ->
-                // Anchor 3D object to current place
-                modelNode.value?.anchor()
+            onTap = { hitResult ->
+                // Anchor 3D object to current place, if it is not anchored yet
+                if (!modelPlaced.value) {
+                    modelNode.value?.anchor()
+                    modelPlaced.value = true
+                    nodePosition.value = hitResult.hitPose.position.toVector3()
+                }
+                // Otherwise, check if already placed model was hit, and display info
+                else if (modelPlaced.value && Vector3Distance(hitResult.hitPose.position.toVector3(), nodePosition.value) < objectTapRadius) {
+                    Log.d("DEBUG", "object hit")
+                    showPopup.value = true
+                }
             }
         )
+    }
+
+    @Composable
+    fun InfoPopup(showPopup: MutableState<Boolean>) {
+        AlertDialog(
+            icon = {
+                Icon(Icons.Filled.Info, contentDescription = "Info Icon")
+            },
+            title = {
+                Text(getString(R.string.placeholder_title))
+            },
+            text = {
+                Text(getString(R.string.placeholder_string_long))
+            },
+            onDismissRequest = {
+                showPopup.value = false
+            },
+            confirmButton = {
+                Button(
+                    onClick = {
+                        showPopup.value = false
+                    }
+                ) {
+                    Text(getString(R.string.ok))
+                }
+            },
+            dismissButton = {
+                Button(
+                    onClick = {
+                        showPopup.value = false
+                    }
+                ) {
+                    Text(getString(R.string.dismiss))
+                }
+            }
+        )
+    }
+
+    fun Vector3Distance(a: Vector3, b: Vector3): Float {
+        return Vector3.subtract(a, b).length()
     }
 }
