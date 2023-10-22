fc51101aa0f538e667fce9be35eb4e87fab53ba1
Reetu Kontio <reetu.kontio@gmail.com>
2023-10-22 12:02:29 +0300

    build apk

--- app/src/main/java/com/example/thinglink_ar_demo/MainActivity.kt
+++ app/src/main/java/com/example/thinglink_ar_demo/MainActivity.kt
@@ -130,8 +130,8 @@ class MainActivity : ComponentActivity() {
             nodes = arNodes,
             planeRenderer = true,
             onCreate = {
-                it.lightEstimationMode = LightEstimationMode.DISABLED
-                it.planeRenderer.isShadowReceiver = false
+                it.lightEstimationMode = LightEstimationMode.AMBIENT_INTENSITY
+                it.planeRenderer.isShadowReceiver = true
                 modelNode.value =
                     ArModelNode(it.engine, placementMode = PlacementMode.INSTANT).apply {
                         loadModelGlbAsync(
